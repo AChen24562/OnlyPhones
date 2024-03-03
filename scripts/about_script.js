ed1fee5bcc4c66daaa8e180eabc6b1df5d1e3c62
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2024-03-03 04:40:16 -0500

    About Page Prelim

--- scripts/about_script.js
+++ scripts/about_script.js
@@ -1,6 +1,15 @@
 const nav = document.querySelector('.nav');
 window.addEventListener('scroll', fixNav);
 
+// FAQ Buttons
+const toggles = document.querySelectorAll('.faq-toggle');
+
+toggles.forEach(toggle =>{
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active');
+    })
+})
+
 function fixNav() {
     if(window.scrollY > nav.offsetHeight){
         nav.classList.add('active');
